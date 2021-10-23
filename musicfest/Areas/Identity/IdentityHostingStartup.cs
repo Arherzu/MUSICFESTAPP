ec6c4df0880061d74964ae8f01f0a496f3ecf3ad
Danny Flores <floresfredj@gmail.com>
2021-10-23 03:14:05 -0500

    modificar login,etc

---
+++ musicfest/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using musicfest.Data;
+
+[assembly: HostingStartup(typeof(musicfest.Areas.Identity.IdentityHostingStartup))]
+namespace musicfest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- musicfest/Startup.cs
+++ musicfest/Startup.cs
@@ -12,7 +12,7 @@ using musicfest.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 namespace musicfest
 {
     public class Startup
@@ -27,9 +27,13 @@ namespace musicfest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+                //options.UseSqlite(
+                    //Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(
+                    Configuration.GetConnectionString("PostgressConnection")));  
+
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
